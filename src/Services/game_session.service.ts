daefcd6352cb1c5f33afc9c90cf5404bbfa7f534
Jakub Chodziński <xapixowy@gmail.com>
2024-10-27 14:45:58 +0100

    start working on game session service

--- src/Services/game_session.service.ts
+++ src/Services/game_session.service.ts
@@ -1,88 +1,70 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Character } from '@Entities/character.entity';
-// import { DataSource, Repository } from 'typeorm';
-// import { CharacterDto } from '@DTOs/character.dto';
-// import { CreateCharacterRequest } from '@Requests/Character/create-character.request';
-// import { UpdateCharacterRequest } from '@Requests/Character/update-character.request';
-// import { NotFoundException } from '@Exceptions/not-found.exception';
-// import { ConfigService } from '@nestjs/config';
-// import { AppConfig } from '../Config/app.config';
-// import { Language } from '@Enums/language';
-// import { GameSession } from '@Entities/game-session.entity';
-//
-// @Injectable()
-// export class GameSessionService {
-//   appLanguage: Language;
-//
-//   constructor(
-//     @InjectRepository(GameSession)
-//     private characterRepository: Repository<GameSession>,
-//     private dataSource: DataSource,
-//     private configService: ConfigService,
-//   ) {
-//     this.appLanguage = this.configService.get<AppConfig>('app').language;
-//   }
-//
-//   async findAll(language?: Language): Promise<CharacterDto[]> {
-//     const characters = await this.characterRepository.find();
-//     return characters.map((character) =>
-//       CharacterDto.fromEntity(
-//         language ? this.getTranslatedCharacter(character, language) : character,
-//       ),
-//     );
-//   }
-//
-//   async findOne(id: number, language?: Language): Promise<CharacterDto> {
-//     const existingCharacter = await this.characterRepository.findOneBy({ id });
-//     if (!existingCharacter) {
-//       throw new NotFoundException();
-//     }
-//     return CharacterDto.fromEntity(
-//       language
-//         ? this.getTranslatedCharacter(existingCharacter, language)
-//         : existingCharacter,
-//     );
-//   }
-//
-//   async add(characterRequest: CreateCharacterRequest): Promise<CharacterDto> {
-//     const character = this.characterRepository.create({
-//       ...characterRequest,
-//       locale: this.appLanguage,
-//     });
-//     return this.dataSource.transaction(async (manager) =>
-//       CharacterDto.fromEntity(await manager.save(character)),
-//     );
-//   }
-//
-//   async edit(
-//     id: number,
-//     characterRequest: UpdateCharacterRequest,
-//   ): Promise<CharacterDto> {
-//     return await this.dataSource.transaction(async (manager) => {
-//       const existingCharacter = await manager.findOneBy(Character, { id });
-//       if (!existingCharacter) {
-//         throw new NotFoundException();
-//       }
-//       manager.merge(Character, existingCharacter, {
-//         ...characterRequest,
-//         updated_at: new Date(),
-//       });
-//       return CharacterDto.fromEntity(
-//         await manager.save(Character, existingCharacter),
-//       );
-//     });
-//   }
-//
-//   async remove(id: number): Promise<CharacterDto> {
-//     return this.dataSource.transaction(async (manager) => {
-//       const existingCharacter = await manager.findOneBy(Character, { id });
-//       if (!existingCharacter) {
-//         throw new NotFoundException();
-//       }
-//       return CharacterDto.fromEntity(
-//         await manager.remove(Character, existingCharacter),
-//       );
-//     });
-//   }
-// }
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Character } from '@Entities/character.entity';
+import { DataSource, Repository } from 'typeorm';
+import { CharacterDto } from '@DTOs/character.dto';
+import { CreateCharacterRequest } from '@Requests/Character/create-character.request';
+import { UpdateCharacterRequest } from '@Requests/Character/update-character.request';
+import { NotFoundException } from '@Exceptions/not-found.exception';
+import { GameSession } from '@Entities/game-session.entity';
+import { GameSessionDto } from '@DTOs/game-session.dto';
+
+@Injectable()
+export class GameSessionService {
+  constructor(
+    @InjectRepository(GameSession)
+    private gameSessionRepository: Repository<GameSession>,
+    private dataSource: DataSource,
+  ) {}
+
+  async findOne(token: string): Promise<GameSessionDto> {
+    const existingSession = await this.gameSessionRepository.findOne({
+      where: { token },
+    });
+    if (!existingSession) {
+      throw new NotFoundException();
+    }
+    return GameSessionDto.fromEntity(existingSession);
+  }
+
+  async add(characterRequest: CreateCharacterRequest): Promise<CharacterDto> {
+    const character = this.characterRepository.create({
+      ...characterRequest,
+      locale: this.appLanguage,
+    });
+    return this.dataSource.transaction(async (manager) =>
+      CharacterDto.fromEntity(await manager.save(character)),
+    );
+  }
+
+  async edit(
+    id: number,
+    characterRequest: UpdateCharacterRequest,
+  ): Promise<CharacterDto> {
+    return await this.dataSource.transaction(async (manager) => {
+      const existingCharacter = await manager.findOneBy(Character, { id });
+      if (!existingCharacter) {
+        throw new NotFoundException();
+      }
+      manager.merge(Character, existingCharacter, {
+        ...characterRequest,
+        updated_at: new Date(),
+      });
+      return CharacterDto.fromEntity(
+        await manager.save(Character, existingCharacter),
+      );
+    });
+  }
+
+  async remove(id: number): Promise<CharacterDto> {
+    return this.dataSource.transaction(async (manager) => {
+      const existingCharacter = await manager.findOneBy(Character, { id });
+      if (!existingCharacter) {
+        throw new NotFoundException();
+      }
+      return CharacterDto.fromEntity(
+        await manager.remove(Character, existingCharacter),
+      );
+    });
+  }
+}
